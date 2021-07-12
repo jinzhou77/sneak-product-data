211413f14a6b5236b4aa83324d56fc47fca1fc32
Jin <Jin@Jins-MacBook-Pro.local>
2021-07-12 02:01:49 -0500

    able to insert all data but need to scale up the DB size

--- insert_table.py
+++ insert_table.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime
 import math
 
-sneaker_detail_url = "http://127.0.0.1:8080/api/sneakers/"
+sneaker_detail_url = "http://localhost:8080/api/sneakers/"
 
 def isnan(value):
     try:
@@ -13,27 +13,28 @@ def isnan(value):
         return False
         
 def post_all_sneakers(brandname):
-    df = pd.read_csv('./data/sneakers/adidas/yeezy/output.csv')
+    failed = 0
+    df = pd.read_csv('./data/sneakers/adidas/yeezy/sneaker_output.csv')
     for index, row in df.iterrows():
         url = row['url']
-        brandname = brandname
-        stylename = row['name']
-        imagepath = row['image_path']
+        brand_name = brandname
+        style_name = row['name']
+        image_path = row['image_path']
 
-        retailprice = None
+        retail_price = None
         if isnan(row['retail_price']) == False :
-            retailprice = float(row['retail_price'][1:])
+            retail_price = float(row['retail_price'][1:])
 
-        numberofsale = 0
+        number_of_sale = 0
         if math.isnan(float(row['number_of_sales'])) == False:
-            numberofsale = int(row['number_of_sales'])
+            number_of_sale = int(row['number_of_sales'])
 
-        averagesaleprice = None
+        average_sale_price = None
         if isnan(row['average_sale_price']) == False:
             avgPrice = row['average_sale_price'].replace(',', '')
-            averagesaleprice = float(avgPrice[1:])
+            average_sale_price = float(avgPrice[1:])
      
-        stylecode = row['style_code']
+        style_code = row['style_code']
         colorway = row['colorway']
         ticker = row['ticker']
         datetime_obj=None
@@ -41,19 +42,24 @@ def post_all_sneakers(brandname):
             datetime_obj = datetime.strptime(row['release_date'], '%m/%d/%Y')
         payload = {
             'url':url,
-            'brandname': brandname,
-            'stylename': stylename,
-            'imagepath': imagepath,
-            'retailprice': retailprice,
-            'numberofsale': numberofsale,
-            'averagesaleprice': averagesaleprice,
-            'stylecode': stylecode,
+            'brand_name': brand_name,
+            'style_name': style_name,
+            'image_path': image_path,
+            'retail_price': retail_price,
+            'number_of_sale': number_of_sale,
+            'average_sale_price': average_sale_price,
+            'style_code': style_code,
             'colorway': colorway,
             'ticker': ticker,
-            'releasedate': datetime_obj
+            'release_date': datetime_obj
         }
         res = requests.post(url=sneaker_detail_url, data = payload)
         print(res.status_code)
+        if(res.status_code != 201):
+            failed+=1
+    print('Failed Post: ')
+    print(failed) 
+    print('Total Number of Trades: ')
     print(len(df))
 
 def main():
